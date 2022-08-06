bb116a8cc2ad46068d862bb3a8c6c74a2b607a19
aparnasj-sj <61081490+aparnasj-sj@users.noreply.github.com>
2022-08-06 19:09:59 +0530

    Update extended_euclidian_algo.cpp

--- extended_euclidian_algo.cpp
+++ extended_euclidian_algo.cpp
@@ -10,3 +10,10 @@ int gcd(int a, int b, int& x, int& y) {
     y = x1 - y1 * (a / b);
     return d;
 }
+/*
+In main we can call  gcd fun 
+if gcd==1 
+only them a has a modular mul inv wrt b 
+else no inv wrt b
+note: x and u are pass by reference here 
+*/
